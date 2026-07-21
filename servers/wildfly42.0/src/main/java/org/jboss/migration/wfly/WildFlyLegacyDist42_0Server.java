c569f968551ef8af1a3ec0ebac13d0b79bea244f
Eduardo Martins <emartins@redhat.com>
2026-07-21 02:51:48 +0100

    [CMTOOL-412] Add support for migrations to WildFly 42.0

---
+++ servers/wildfly42.0/src/main/java/org/jboss/migration/wfly/WildFlyLegacyDist42_0Server.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright The WildFly Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.jboss.migration.wfly;
+
+import org.jboss.migration.core.ProductInfo;
+import org.jboss.migration.core.env.MigrationEnvironment;
+import org.jboss.migration.wfly10.ServiceLoaderWildFlyServerMigrations10;
+import org.jboss.migration.wfly10.WildFlyServerMigrations10;
+
+import java.nio.file.Path;
+import java.util.ServiceLoader;
+
+/**
+ * The WildFly 42.0 Legacy Dist {@link org.jboss.migration.core.Server}.
+ * @author emmartins
+ */
+public class WildFlyLegacyDist42_0Server extends WildFly42_0Server {
+
+    private static final WildFlyServerMigrations10 SERVER_MIGRATIONS = new ServiceLoaderWildFlyServerMigrations10<>(ServiceLoader.load(WildFly42_0ServerMigrationProvider.class));
+
+    public WildFlyLegacyDist42_0Server(String migrationName, ProductInfo productInfo, Path baseDir, MigrationEnvironment migrationEnvironment) {
+        super(migrationName, productInfo, baseDir, migrationEnvironment);
+    }
+
+    @Override
+    protected WildFlyServerMigrations10 getMigrations() {
+        return SERVER_MIGRATIONS;
+    }
+
+    @Override
+    public DistributionType getDistributionType() {
+        return DistributionType.DIST;
+    }
+
+    @Override
+    public TechnologyType getTechnologyType() {
+        return TechnologyType.LEGACY;
+    }
+}
